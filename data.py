825f73ed9aa3e7680039cde441fe725381b8c23a
Ruben <rbntgn@gmail.com>
2020-03-20 13:12:33 +0100

    Removed blanks in keys

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 aminoacids = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
      'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N', 
      'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
-     'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M', "DT ":"Z", "DG ":"X", "DC ":"Ñ", 
-     "DA ":"O"}
+     'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M', "DT":"Z", "DG":"X", "DC":"U", 
+     "DA":"O"}
