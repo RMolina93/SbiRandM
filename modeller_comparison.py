eab8dc9d2204fbe95f2e3830e0070021762c2768
miguel <dieguez2410@gmail.com>
2020-03-24 17:45:55 +0100

    Merge branch 'master' of /Users/miguel/PycharmProjects/Structural_Project with conflicts.

--- modeller_comparison.py
+++ modeller_comparison.py
@@ -1,194 +1,56 @@
 
-from modeller import *              
-from modeller.automodel import *    
+# Homology modeling with multiple templates
+from modeller import *              # Load standard Modeller classes
+#from modeller.automodel import *    # Load the automodel class
 import argparse
 import glob, os, argparse, warnings
 from Bio.PDB import *
 from Bio.PDB.PDBParser import PDBParser
-from Bio import SeqIO
-from Bio import pairwise2
-import shutil
-
 from data import aminoacids
-from models import Protein_Interaction, Chain, Query
-import difflib
 
 warnings.filterwarnings("ignore")
+'''
+parser = argparse.ArgumentParser(description='Complexes modelling application.')
 
-def parse_arguments():
+parser.add_argument('-d', action="store", required =True, dest="folder", help="Folder with all the PDBs of the interactions complex")
+parser.add_argument('-fasta', action="store", required=False, dest="fasta_seq", help="Fasta sequence of the full complex.")
+parser.add_argument('-output', action="store", required=True, dest="output_folder", help="Output dir to store the results")
 
+args = vars(parser.parse_args())
+'''
+def block_alignment(pir_file):
     """
-    Parsing of arguments. 
-    -d : Folder with PDB's Pairwise Interactions
-    -fasta: File with the fasta you want to model
-    -output: Folder to put the output
+    This function should return the alignment with the DNA BLOCKED for modeller.
     """
+    return None
 
-    parser = argparse.ArgumentParser(description='Complexes modelling application.')
 
-    parser.add_argument('-d', action="store", required =True, dest="folder", help="Folder with all the PDBs of the interactions complex")
-    parser.add_argument('-fasta', action="store", required=True, dest="fasta_seq", help="Fasta sequence of the full complex.")
-    parser.add_argument('-output', action="store", required=True, dest="output_folder", help="Output dir to store the results")
-
-    args = vars(parser.parse_args())
-    return args
-
-def fasta_to_object(fasta):
-
-    """
-    This function takes a fasta file with several chains, and parse it into a
-    query object. Also changes the DNA chains to Placeholder characters.
-
-    @input folder - File with Fasta
-    @output - Query object.
-    """
-
-    record_dict = SeqIO.to_dict(SeqIO.parse(fasta, "fasta"))
-    query = Query(name=(os.path.splitext(os.path.basename(fasta))[0]))
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    index = 0
-    for sequence in record_dict.keys():
-        chain = Chain(name = alphabet[index], sequence = record_dict[sequence].seq, first_residue = 1, last_residue = len(record_dict[sequence].seq))
-        chain.dna_to_placeholder()
-        query.add_chain(chain)
-        index += 1
-    return query
-
-def create_models(folder):
-
+def obtain_sequence(folder):
     """
     This function takes a folder that contains the complexes of the pairwise interactions, 
-    parse them and return a list of Protein_Interaction objects with information 
-    about the location and the sequences.
+    parse them and return a JSOn with information about the location and the sequences.
 
     @input folder - Folder where you store the pairwise complexes of the protein
-    @output - List of Protein_Interaction objects
+    @output - JSON formatted variable with -  Proteins : chains : complex_path_file / sequence
     """
 
-    list_of_interactions = list()
+    templates_dict = dict()
     parser = PDBParser(PERMISSIVE=1)
     for pdb_file in glob.glob(os.path.join(folder,"*.pdb")):
         pdb_name = os.path.basename(pdb_file)
-        protein = Protein_Interaction(name = pdb_name, path = pdb_file)
+        templates_dict[pdb_name] = {"path":pdb_file, "name":pdb_name}
         structure = parser.get_structure('Complex', pdb_file)
 
         for pdb_chain in structure[0]:
-            sequence = ""
-            
+            templates_dict[pdb_name][pdb_chain.id] = ""
             for residue in pdb_chain:
-                sequence += aminoacids[residue.get_resname().strip()]
-            protein.add_chain(Chain(name = pdb_chain.get_id(), sequence = sequence, first_residue = list(pdb_chain)[0].get_id()[1], last_residue = list(pdb_chain)[-1].get_id()[1]))
-
-        list_of_interactions.append(protein)
-    
-    return list_of_interactions
-
-def check_similarity(query, interactions_list):
-
-    """
-    This function takes a query object and a list of Pairwise interaction objects, and
-    returns the pairs of chains that have a similarity ratio on sequence over 73%
-    """
-    for chain in query.chains:
-        for protein in interactions_list:
-            for protein_chain in protein.chains:
-                #print (chain.sequence.strip(), "/////", str(protein_chain.sequence)) 
-                if difflib.SequenceMatcher(None,chain.sequence.strip(), str(protein_chain.sequence)).ratio() >0.73 :
-                    protein_chain.originalChain = chain.name
-                    #print ("These two chains match", chain.name, protein.name, protein_chain.name)
-
-def generate_alignment(query, interactions, output_folder):
-
-    if not os.path.isdir(output_folder): os.mkdir(output_folder)
-
-    with open(os.path.join(output_folder, "alignment.pir"), "w") as output:
-
-        query_info = list()
-        #QUERY
-        output.write(">P1;" + query.name.strip() + "\n")
-        output.write("sequence:" + query.name + ": 1:" + query.chains[0].name + " :" + str(len(query.chains[0].sequence)) + ":" + query.chains[-1].name + ": : :-1.0:-1.0\n")
-        
-        for query_chain in query.chains:
-
-            if any((c in "UOZX") for c in query_chain.sequence):
-                if query_chain.name == query.chains[-1].name:  
-                    output.write("." * len(query_chain.sequence) + "*\n\n")
-                else:                    
-                    output.write("." * len(query_chain.sequence) + "\n/\n")
-            else:
-                if query_chain.name == query.chains[-1].name:  
-                    output.write(query_chain.sequence.strip() + "*\n\n") 
-                else:                    
-                    output.write(query_chain.sequence.strip() + "\n/\n") 
-
-        
-        for interaction in interactions:
-            output.write(">P1;" + interaction.name + "\n")
-            output.write("structureX:" + interaction.name + ":" + interaction.chains[0].first_aminoacid + ":A:" + interaction.chains[1].last_aminoacid + ":B: : :-1.0:-1.0\n")
-            for chain in query.chains:
-                if interaction.chains[0].originalChain == chain.name: # CHAIN A OF INTERACTION
-                    
-                    if any((c in "UOZX") for c in interaction.chains[0].sequence):
-
-                        if chain.name == query.chains[-1].name:  
-                            output.write("." * len(chain.sequence) + "*\n\n")
-                        else:                    
-                            output.write("." * len(chain.sequence) + "\n/\n")
-                    else : 
-                        alignments = pairwise2.align.globalms(chain.sequence, interaction.chains[0].sequence,  2, -1, -30, -10)
-                        output.write(alignments[0][1] + "\n/\n")
-
-                elif interaction.chains[1].originalChain == chain.name: # CHAIN B OF INTERACTION
-                    if any((c in "UOZX") for c in interaction.chains[1].sequence):
-                        if chain.name == query.chains[-1].name: 
-                            output.write("." * len(chain.sequence) + "*\n\n")
-                        else:
-                            output.write("." * len(chain.sequence) + "\n/\n")
-
-                    else: 
-                        alignments = pairwise2.align.globalms(chain.sequence, interaction.chains[1].sequence,  2, -1, -30, -10)
-                        if chain.name == query.chains[-1].name:  
-                            output.write(alignments[0][1] + "*\n\n")
-                        else:                    
-                            output.write(alignments[0][1] + "\n/\n")
-                else:
-                    if chain.name == query.chains[-1].name: 
-                        output.write("-" * len(chain.sequence) + "*\n\n")
-                    else:
-                        output.write("-" * len(chain.sequence) + "\n/\n")
-        
-def make_model(output_folder, interaction_pdb_folder, fasta):
-    #detect pir file
-    templates = list()
-    for interaction_file in os.listdir(interaction_pdb_folder):
-        templates.append(interaction_file)
-        shutil.copyfile( os.path.join(interaction_pdb_folder, interaction_file) , os.path.join(output_folder, interaction_file) )
-
-    os.chdir(output_folder)
-    log.verbose()    # request verbose output
-    env = environ()  # create a new MODELLER environment to build this model in
-
-    # directories for input atom files
-    env.io.atom_files_directory = ['.', '../atom_files']
-    pir_file = glob.glob("*.pir")[0]
-
-    a = automodel(env,
-                alnfile  = pir_file, # alignment filename
-                knowns   = tuple(templates),     # codes of the templates
-                sequence = str(os.path.splitext(os.path.basename(fasta))[0].strip()))       
-
-    a.starting_model= 1                 # index of the first model
-    a.ending_model  = 2                 # index of the last model
-
-    a.make()                            # do the actual homology modeling
+                if residue.get_resname() != 'UNK': # Por si en el pdb hay residuos desconocidos. SOLUCION A ARREGLAR
+                    templates_dict[pdb_name][pdb_chain.id] += aminoacids[residue.get_resname()]
+    print(templates_dict)
+    return templates_dict
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
+#    obtain_sequence(args['folder'])
 
-    args = parse_arguments()
-    query = fasta_to_object(args['fasta_seq'])
-    interactions = create_models(args['folder'])
-    check_similarity(query, interactions)
-    generate_alignment(query, interactions, args['output_folder'])
-    make_model(args['output_folder'], args['folder'], args['fasta_seq'])
 
--- protein_comparison.py
+++ protein_comparison.py
@@ -3,54 +3,115 @@ from Bio.PDB import PDBParser
 from Bio.PDB import Polypeptide
 import numpy as np
 import subprocess
+import modeller_comparison as mc
 
 
-proteinPath = "/Users/miguel/PycharmProjects/Structural_Project/structures/test/1gzx.pdb"
+pdbExtension = ".pdb"
+proteinName = "1gzx"
+proteinPath = "/Users/miguel/PycharmProjects/Structural_Project/structures/test/"
 scriptPath = "/Users/miguel/PycharmProjects/Structural_Project/modules/scripts/PDBtoSplitChain2.pl"
+stdoutPath = "/Users/miguel/PycharmProjects/Structural_Project/"
 
-def superimposition():
-   structure = PDBParser().get_structure("3t72", "structures/3t72.pdb")
+
+def superimpose():
    
-   for x in structure.get_atoms():
-      print(x.get_name())
 
-   ref_model = structure[0]
-   for alt_model in structure:
-      ref_atoms = []
-      alt_atoms = []
-      for (ref_chain , alt_chain) in zip(ref_model , alt_model):
-         for ref_res , alt_res , amino , allow in zip(ref_chain , alt_chain , seq_str , use):
-            assert ref_res.resname == alt_res.resname
-            assert ref_res.id == alt_res.id
-            assert amino == Polypeptide.three_to_one(ref_res.resname)
-            if allow:
-               # CA = alpha carbon
-               ref_atoms.append(ref_res['CA'])
-               alt_atoms.append(alt_res['CA'])
-
-      # Align these paired atom lists:
-      super_imposer = SVDSuperimposer()
-      super_imposer.set_atoms(ref_atoms , alt_atoms)
+   respuesta = mc.obtain_sequence('structures/6gmh/TMP/')
    
-      if ref_model.id == alt_model.id:
-         # Check for self/self get zero RMS, zero translation
-         # and identity matrix for the rotation.
-         assert np.abs(super_imposer.rms) < 0.0000001
-         assert np.max(np.abs(super_imposer.rotran[1])) < 0.000001
-         assert np.max(np.abs(super_imposer.rotran[0]) - np.identity(3)) < 0.000001
-      else:
-         # Update the structure by moving all the atoms in
-         # this model (not just the ones used for the alignment)
-         super_imposer.apply(alt_model.get_atoms())
+   str1 = list(respuesta.keys())[0]
+   str2 = list(respuesta.keys())[0]
 
-
-#superimposition()
-def decomposeInChains(proteinPath, scriptPath):
-   #This function uses the perl script to decompose a protein in its chains
+   sup = SVDSuperimposer()
    
-   # Result: Fastas and PDBs of the different chains
-   # Format: <name_Of_Protein><Chain (A,B...)>.<fa>/<pdb>
+   sup.set_atoms
+if __name__ == "__main__":
+   superimpose()
    
-   var = proteinPath
-   pipe = subprocess.Popen(["perl", scriptPath ,"-i" ,var, "-o", "/Users/miguel/PycharmProjects/Structural_Project/1gzx"],stdout=subprocess.PIPE)
-   pipe.communicate()
+   
+   '''
+   if options.verbose:
+      print('\nSuperimposing %s over chain %s' % (str2 , node.get_chain()))
+   chain1 = node.get_chain()
+   str2_copy = [x.copy() for x in str2]
+   node_chain_copy = str1[0][chain1].copy()
+   chain_str2 = i.copy()
+   
+   # Trim the chains so that they have the same number of atoms to superimpose
+   trim_to_superimpose(node_chain_copy , chain_str2)
+   atoms_chain1 = [atom for atom in list(node_chain_copy.get_atoms()) if atom.get_id() == 'CA' or atom.get_id() == 'P']
+   atoms_chain2 = [atom for atom in list(chain_str2.get_atoms()) if atom.get_id() == 'CA' or atom.get_id() == 'P']
+   sup = Superimposer()
+   
+   # Superimpose the chains
+   sup.set_atoms(atoms_chain1 , atoms_chain2)
+   
+   # Select the chains that haven't been superimposed
+   if not homodimer:
+      other_chain2 = [x for x in str2_copy if x.get_id() != chain_str2.get_id()][0]
+      other_chain2_original = [x for x in str2 if x.get_id() != chain_str2.get_id()][0]
+   else:
+      other_chain2 = str2_copy[1]
+      other_chain2_original = str2[1]
+   
+   # Apply the rotation matrix to the chain we want to add to the complex
+   sup.apply(other_chain2)
+   
+   # Assess if there is a clash between the chain we want to add and the others
+   if not get_clash_chains(str1 , other_chain2 , chain1 , options):
+      
+      # add the chain to the macrocomplex
+      other_chain2.id = len(complex_id.get_nodes()) + 1
+      similar_seq[other_chain2] = similar_seq[other_chain2_original]
+      complex_id.add_node(other_chain2 , node , str2)
+      str1[0].add(other_chain2)
+      
+      if options.intensive:
+         interaction_finder(str1 , other_chain2.get_id() , complex_id , node , options)
+      
+      return True
+   else:
+      return 'clash'
+'''
+'''
+def trim_to_superimpose(chain1 , chain2):
+   # Todo: mirar que pasa si consideras una similaridad del 100% para que sean similar seqs
+   """
+   Takes two chains and removes the residues that do not have a match in the sequence alignment.
+   :param chain1: first chain.
+   :param chain2:  second chain.
+   :return: returns chain 1 and 2 trimmed so that they have the same atom length.
+   """
+   seq1 = get_sequence_from_chain(chain1)
+   seq2 = get_sequence_from_chain(chain2)
+   
+   alignment = pairwise2.align.globalxx(seq1 , seq2)
+   
+   score = alignment[0][2]
+   length = max(len(seq1) , len(seq2))
+   ident_perc = score / length
+   
+   # in principle, there will not get here two chains that do not have more than 95% of similarity
+   if ident_perc > 0.95:
+      
+      seq1_array = list(alignment[0][0])  # Storing the alignment sequences as arrays
+      seq2_array = list(alignment[0][1])
+      seq1_numeric = get_numeric_array(alignment[0][0])  # Storing those sequences with numbers instead of residuals
+      seq2_numeric = get_numeric_array(alignment[0][1])
+      to_delete_from_1 = []  # To avoid modifying a list while iterating through it we will store the elements to
+      to_delete_from_2 = []  # remove in these lists
+      pairs1 = zip(seq1_array , seq2_numeric)  # we pair the sequnce alignment with the numeric list of the other chain
+      for pair in pairs1:
+         if pair[0] == '-':  # If there is a gap in the sequence, we will remove the nth residue of the other chain
+            to_delete_from_2.append(list(chain2.get_residues())[pair[1]].get_id())
+      
+      pairs2 = zip(seq2_array , seq1_numeric)
+      for pair in pairs2:
+         if pair[0] == '-':
+            to_delete_from_1.append(list(chain1.get_residues())[pair[1]].get_id())
+      
+      for residue_to_delete in to_delete_from_1:  # Removing the residuals from the sequences
+         chain1.__delitem__(residue_to_delete)
+      
+      for residue_to_delete in to_delete_from_2:  # Removing the residuals from the sequences
+         chain2.__delitem__(residue_to_delete)
+         '''
