09fcd2bdc701e6c9428cb693fb16c06d76b37e4c
Ruben <rbntgn@gmail.com>
2020-04-03 12:04:31 +0200

    Documented file

--- sbiRandM/sbiRandM/GUI/mainWindow.py
+++ sbiRandM/sbiRandM/GUI/mainWindow.py
@@ -9,48 +9,41 @@ import logging
 
 os.environ['TK_SILENCE_DEPRECATION'] = "1"
 
-class ventana():
+class window():
+    """
+    This class is defined as the main window of the Graphical User Interface.
+    """
    
     def __init__(self , master):
+
         logging.basicConfig(filename='example.log' , level=logging.DEBUG)
-        ### MODELLER TAB ZONE
       
         self.lblModPDBPath = ttk.Label(master, text="Introduce the path to the PDB directory")
-      
         self.lblModPDBPath.grid(column=0, row=0)
       
         self.lblModFastaPath = ttk.Label(master, text="Introduce the path to the fasta file")
-      
         self.lblModFastaPath.grid(column=0, row=2)
       
         self.lblModOutputPath = ttk.Label(master, text="Introduce the path to the output directory")
-      
         self.lblModOutputPath.grid(column=0, row=4)
       
         self.txtModPDBPath = ttk.Entry(master,width=40)
-
         self.txtModPDBPath.grid(column=1, row=0)
       
         self.txtModFastaPath = ttk.Entry(master,width=40)
-      
         self.txtModFastaPath.grid(column=1, row=2)
       
         self.txtModOutputPath = ttk.Entry(master,width=40)
-      
         self.txtModOutputPath.grid(column=1, row=4)
         
         self.lblOut = ttk.Label(master , text="You can follow the results in the Terminal!")
-
         self.lblOut.grid(column=1 , row=9)
         
         self.var = IntVar()
-
         self.radioMod = ttk.Radiobutton(master , text="Use Modeller" , variable=self.var , value=1)
         self.radioMod.grid(column=1 , row=6)
-
         self.radioSup = ttk.Radiobutton(master , text='Use Superimposition' , variable=self.var , value=2)
         self.radioSup.grid(column=0 , row=6)
-        
         self.var.set(2)
 
         self.verbose = BooleanVar()
@@ -58,27 +51,25 @@ class ventana():
         self.verb.grid(column=2, row=6)
 
         self.btnMod = ttk.Button(master , text='Run!' , command=self.clicked, width=20)
-
         self.btnMod.grid(column=3 , row=6)
 
         self.btn1 = ttk.Button(master , text='Browse PDB' , command=self.browsePDB , width=10)
-   
         self.btn1.grid(column=3 , row=0)
+
         self.btn2 = ttk.Button(master , text='Browse fasta' , command=self.browseFasta , width=10)
-   
         self.btn2.grid(column=3 , row=2)
+
         self.btn3 = ttk.Button(master , text='Select output' , command=self.browseOutput , width=10)
-   
         self.btn3.grid(column=3 , row=4)
 
-   ### FUNCTION ZONE
+
     def clicked(self):
-        #Codigo de ejecucion modeller
         args = dict()
         if (self.txtModPDBPath.get() == "" or self.txtModFastaPath.get() == "" or self.txtModOutputPath.get() == ""):
            messagebox.showinfo("Error (But don't worry!)" ,
                                "You didn't enter a path or file for the arguments! Please feel free to try again.")
            self.clear()
+
         else:
    
            args['folder'] = self.txtModPDBPath.get()
@@ -87,11 +78,9 @@ class ventana():
            args['verbose'] = self.verbose.get()
            try:
                 if self.var.get() == 2:
-                   #Por superimposicion
-                   mainSuperimp(args)
+                   mainSuperimp(args)   #Call superimposition-based approach
                 elif self.var.get() == 1:
-                   
-                   mainMod(args)
+                   mainMod(args)        #Call Modeling-based approach
 
            except FileNotFoundError:
                 messagebox.showinfo("Error (But don't worry!)" , "It seems like one of the paths that you introduced is not valid. Please try again!")
@@ -116,41 +105,63 @@ class ventana():
                 self.clear()
 
     def clear(self):
+        """
+        This function clears the paths in the GUI interface
+        """
         self.txtModPDBPath.delete(0 , 'end')
         self.txtModFastaPath.delete(0 , 'end')
         self.txtModOutputPath.delete(0 , 'end')
 
-    ### END OF FUNCTION ZONE
 
     def browsePDB(self):
+        """
+        This function makes a System Call to let the user
+        browse for a folder. 
+        Then it inserts the directory into the GUI field.
+        """
         
         directory = filedialog.askdirectory()
         self.txtModPDBPath.delete(0 , 'end')
         self.txtModPDBPath.insert(0,directory)
 
     def browseFasta(self):
+        """
+        This function makes a System Call to let the user
+        browse for a fasta file. 
+        Then it inserts the file into the GUI field.
+        """
    
        file = filedialog.askopenfilename()
        self.txtModFastaPath.delete(0 , 'end')
        self.txtModFastaPath.insert(0,file )
 
     def browseOutput(self):
+        """
+        This function makes a System Call to let the user
+        browse for an output directory. 
+        Then it inserts the file into the GUI field.
+        """
    
        directory = filedialog.askdirectory()
        self.txtModOutputPath.delete(0 , 'end')
        self.txtModOutputPath.insert(0,directory)
 
     def validate_input(self, input):
+        """
+        This function validates that the inputs
+        in the GUI fields are strings.
+        """
         if type(input) == str:
             return True
         return False
+
+
 if __name__ == "__main__":
+
     print ("Welcome to the GUI Interface of SbiRandM Complex Builder. Please fill the options to proceed.")
     root = Tk()
-    
-    ss = ventana(root)
-    
-    root.title("sbiRandM incredibly awesome complex builder")
+    ss = window(root)
+    root.title("sbiRandM Complex Builder")
     root['bg'] = "#EAEAEA"
     root.resizable(False, False)
     root.mainloop()
